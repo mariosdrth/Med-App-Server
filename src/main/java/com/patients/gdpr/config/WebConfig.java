938f6e55ef6c4a419be85971ee84d5c3e44ce4a0
Marios Papamarkou <mariosdrth@yahoo.gr>
2019-09-13 15:59:38 +0200

    SSL

--- src/main/java/com/patients/gdpr/config/WebConfig.java
+++ src/main/java/com/patients/gdpr/config/WebConfig.java
@@ -1,5 +1,11 @@
 package com.patients.gdpr.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -29,5 +35,30 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://192.168.1.13:8090")
                 .allowedOrigins("http://samplesite.ddns.net:8090")
                 .allowedOrigins("*");
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {@Override
+        protected void postProcessContext(Context context) {
+            SecurityConstraint securityConstraint = new SecurityConstraint();
+            securityConstraint.setUserConstraint("CONFIDENTIAL");
+            SecurityCollection collection = new SecurityCollection();
+            collection.addPattern("/*");
+            securityConstraint.addCollection(collection);
+            context.addConstraint(securityConstraint);
+        }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(8089);
+        return connector;
     }
 }
