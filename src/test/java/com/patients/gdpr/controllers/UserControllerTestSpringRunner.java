5ab022751def22866a2c30221477728ccfa68512
Marios Papamarkou <mariosdrth@yahoo.gr>
2019-10-27 10:51:36 +0100

    Tests

---
+++ src/test/java/com/patients/gdpr/controllers/UserControllerTestSpringRunner.java
@@ -0,0 +1,38 @@
+package com.patients.gdpr.controllers;
+
+import com.patients.gdpr.dto.UserDTO;
+import com.patients.gdpr.model.User;
+import com.patients.gdpr.services.UserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Deprecated
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTestSpringRunner {
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    User user;
+
+    @MockBean
+    UserDTO userDTO;
+
+    @Autowired
+    UserController userController;
+
+    @Test
+    public void loginUserTest() {
+        Mockito.when(userService.loginUser(user)).thenReturn(userDTO);
+        Assert.assertEquals(userDTO, userController.loginUser(user));
+    }
+
+}
